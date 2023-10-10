62bbb7b19f1c82872474b289af986abe87c4c5ce
Yago-Caetano <yago_caetano@outlook.com>
2023-10-09 22:36:04 -0300

    refc - post example

--- samples/post_data.py
+++ samples/post_data.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import requests
 
 ENDPOINT = "localhost:1026"
@@ -9,23 +10,34 @@ with open('sample_dataset.csv', 'r') as csv_file:
 
     # Get the column names from the CSV header
     column_names = csv_reader.fieldnames
-
     # Iterate through each row in the CSV
     for row in csv_reader:
-        # Prepare the data along with column names
-        aux_data = {}
-        for column_name in column_names:
-            aux_data = row[column_name]
-            # Make a POST request to the API
-            api_url = f'http://{ENDPOINT}/v2/entities/urn:ngsi-ld:entity:a7f63730-43c7-4a6b-93c1-8b8903e76dc1/attrs/{column_name}/value'
-            headers = {'fiware-service': 'smart', 'fiware-servicepath':'/', 'Content-Type':'text/plain'}  # Add any required headers
+        try:
+            # Prepare the data along with column names
+            put_dict = {}
 
-            print(f'PUT Topic: {api_url} - PAYLOAD: {aux_data}')
+            aux_data = {}
+            for column_name in column_names:
+                if((len(column_name) < 2) or (column_name == "date")):
+                    continue
+                
+                put_dict[column_name] = {"value": float(row[column_name]), "type":"float"}
+                # Make a POST request to the API
+                #put_list.append(aux_data)
 
-            response = requests.put(api_url, data=aux_data, headers=headers)
+            api_url = f'http://{ENDPOINT}/v2/entities/urn:ngsi-ld:entity:a7f63730-43c7-4a6b-93c1-8b8903e76dc1/attrs'
+            headers = {'fiware-service': 'smart', 'fiware-servicepath':'/', 'Content-Type':'application/json'}  # Add any required headers
+
+            put_payload = json.dumps(put_dict)
+            print(f'PUT Topic: {api_url} - PAYLOAD: {put_payload}')
+
+            response = requests.put(api_url, data=put_payload, headers=headers)
 
             print(f'{response.status_code} {response.text}')
 
+
+        except:
+            pass
         # Handle the API response (e.g., print response status code)
         #print(f"Row {row['id']} - Status Code: {response.status_code}")
 
